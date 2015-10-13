d7c84d8b38dbe0d17bb5698f12df2a9a5912b599
Jubayer <aljubayer@gmail.com>
2015-10-13 23:47:42 +0600

    Admin Telephone and User part integrated

---
+++ codeBase/SignalSystemApp/SignalSystemApp/Controllers/TelephoneController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalSystemApp.Controllers
+{
+    public class TelephoneController : Controller
+    {
+        // GET: Telephone
+        public ActionResult AddTelephone()
+        {
+            return View();
+        }
+
+        public ActionResult PendingComplains()
+        {
+            return View();
+        }
+
+        public ActionResult ResolvedComplains()
+        {
+            return View();
+        }
+    }
+}
