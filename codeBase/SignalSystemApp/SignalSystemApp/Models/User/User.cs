0d0b8e02c77ae6ed80321a291ff6336e6116847a
atiquereza <atiquereza@yahoo.com>
2015-10-17 21:06:56 +0600

    aljubayer Codebase Replaced

---
+++ codeBase/SignalSystemApp/SignalSystemApp/Models/User/User.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using SignalSystem.Libs;
+
+
+using System.Web;
+
+namespace SignalSystemApp.Models.User
+{
+    public class UserInfo
+    {
+        public string BANumber { set; get; }
+        public string Name { set; get; }
+        public string Rank { set; get; }
+        public string Role { set; get; }
+        public string Email { set; get; }
+
+
+    }
+    public class User
+    {
+        DBGateway aGateway = new DBGateway("SignalSystemConnectionString");
+        public bool CreateUser(string baNumber, string name, string rank, string email, string password, string sex, string role, out string outputMessage)
+        {
+
+            if (UserExists(name, email,baNumber,out outputMessage))
+            {   
+                return false;
+            }
+
+            string nonQuery = "insert into users (`UserName`,`UserCredential`,`UserRoleId`) Values(@name,@password,@role);";
+            Hashtable aHashtable = new Hashtable();
+            aHashtable.Add("name", name);
+            aHashtable.Add("password", password);
+            aHashtable.Add("role", Convert.ToInt32(role));
+            aGateway.Insert(nonQuery,aHashtable);
+            int id = Convert.ToInt32(aGateway.Select("SELECT LAST_INSERT_ID();").Tables[0].Rows[0][0]);
+            nonQuery =
+                "INSERT INTO `signalappdb`.`userinfo` (`UserID`, `UserName`, `FullName`, `Email`, `Gender`, `BANumber`,`RankId`) " +
+                "VALUES (@id, @name, @name, @email, @sex, @baNumber,@rank);";
+            aHashtable = new Hashtable();
+            aHashtable.Add("id", id);
+            aHashtable.Add("name", name);
+            aHashtable.Add("email", email);
+            aHashtable.Add("sex", sex);
+            aHashtable.Add("baNumber", baNumber);
+            aHashtable.Add("rank",rank);
+            aGateway.Insert(nonQuery,aHashtable);
+            
+            
+            outputMessage = string.Empty;
+            return true;
+        }
+
+        private bool UserExists(string name, string email,string baNumber, out string outputMessage)
+        {
+            outputMessage = string.Empty;
+            string query = "select * from userinfo where username=@userName or email=@email or BANumber=@baNumber;";
+            Hashtable aHashtable = new Hashtable(){{"userName",name},{"email",email},{"baNumber",baNumber}};
+            DataSet aSet = aGateway.Select(query,aHashtable);
+
+            foreach (DataRow dataRow in aSet.Tables[0].Rows)
+            {
+                if (dataRow["Email"].ToString() == email)
+                {
+                    outputMessage += "Email Exists !";
+                }
+                if (dataRow["UserName"].ToString() == name)
+                {
+                    outputMessage += "Name Exists !";
+                }
+
+                if (dataRow["BANumber"].ToString() == baNumber)
+                {
+                    outputMessage += "BANumber Exists !";
+                }
+            }
+
+            if (outputMessage.Trim().Length == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public List<UserInfo> GetUserInfo()
+        {
+            string query =
+                "select userinfo.UserName,userinfo.Email,userinfo.BANumber,roles.RoleName,menusrank.Value as Rank" +
+                " from users,userinfo,roles,menusrank where "
+                + "users.UserRoleId = roles.ID and users.ID = userInfo.userID and userinfo.RankId=menusrank.id " +
+                ";";
+            DataSet aSet = aGateway.Select(query);
+
+            List<UserInfo> userList = new List<UserInfo>();
+
+            foreach (DataRow dataRow in aSet.Tables[0].Rows)
+            {
+                UserInfo aInfo = new UserInfo();
+                aInfo.BANumber = dataRow["BANumber"].ToString();
+                aInfo.Name = dataRow["UserName"].ToString();
+                aInfo.Email = dataRow["Email"].ToString();
+                aInfo.Rank = dataRow["Rank"].ToString();
+                aInfo.Role = dataRow["RoleName"].ToString();
+                userList.Add(aInfo);
+
+            }
+            
+            return userList;
+        }
+    }
+}
---
+++ codeBase1/SignalSystemApp/SignalSystemApp/Controllers/TelephoneController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SignalSystemApp.Models.Telephone;
+
+namespace SignalSystemApp.Controllers
+{
+    public class TelephoneController : Controller
+    {
+        // GET: Telephone
+
+
+        public ActionResult NewComplain()
+        {
+            return View();
+        }
+
+        public ActionResult SearchActiveTelephoneInfo()
+        {
+
+            string telephoneNumber = Request["search"].ToString();
+
+            Telephone telephone = new Telephone();
+            Dictionary<string,string> userInfo = telephone.GetUserInfo(telephoneNumber);
+
+            if (userInfo.Count == 0)
+            {
+                ViewData["Message"] = "Error! Telephone Number Not in use....";
+                return View("NewComplain");
+            }
+            ViewData["ID"] = userInfo["ID"];
+            ViewData["Name"] = userInfo["Name"];
+            ViewData["BANumber"] = userInfo["BANumber"];
+            ViewData["TelephoneNumber"] = userInfo["NewPhoneNumber"];
+            ViewData["Address"] = userInfo["Address"];
+            ViewData["Rank"] = userInfo["Rank"];
+
+
+            ViewData["Message"] = "Searched Result";
+            return View("NewComplain");
+        }
+
+
+        public ActionResult SubmitComplains()
+        {
+
+            string userId = Request["userId"].ToString();
+
+            string complainTypeId = Request["type"].ToString();
+            string description = Request["description"].ToString();
+
+            Telephone aTelephone = new Telephone();
+            bool success = aTelephone.SubmitNewComplain(userId,description,complainTypeId);
+
+
+            ViewData["Message"] = "Submitted Complains";
+            return View("NewComplain");
+        }
+        public ActionResult AssignTelephone()
+        {
+            string baNumber = Request["baNumber"].ToString();
+            string name = Request["name"].ToString();
+            string rank = Request["rank"].ToString();
+            string telephoneNumber = Request["telephoneNumber"].ToString();
+            string address = Request["address"].ToString();
+            string sex = string.Empty;
+            if (Request["sex"] != null)
+            {
+                sex = Request["sex"].ToString();
+            }
+
+            if (baNumber.Trim().Length == 0 || name.Trim().Length == 0 || rank.Trim().Length == 0 || telephoneNumber.Trim().Length == 0
+               || address.Trim().Length == 0 || sex.Trim().Length == 0)
+            {
+                ViewData["Message"] = "Error! Fields can not left blank";
+                return View("AddTelephone");
+            }
+
+            string outputMessage = string.Empty;
+            Telephone aTelephone = new Telephone();
+            bool success = aTelephone.AssignNewTelephone(baNumber, name, rank, telephoneNumber, address, sex, out outputMessage);
+
+            if (!success)
+            {
+                ViewData["Message"] = outputMessage;
+
+                return View("AddTelephone");
+            }
+
+
+            ViewData["Message"] = "Telephone User Successfully Added";
+
+            return View("AddTelephone");
+        }
+
+        public ActionResult AddTelephone()
+        {
+            return View();
+        }
+
+        public ActionResult PendingComplains()
+        {
+            Telephone telephone = new Telephone();
+            
+            List<Dictionary<string,string>> pendingComplains = new List<Dictionary<string, string>>();
+            pendingComplains = telephone.GetPendingComplains();
+            ViewData["PendingComplains"] = pendingComplains;
+            return View();
+        }
+
+        public ActionResult ResolvedComplains()
+        {
+            Telephone telephone = new Telephone();
+            List<Dictionary<string, string>> resolved = new List<Dictionary<string, string>>();
+            resolved = telephone.GetResolvedComplains();
+            ViewData["Resolved"] = resolved;
+
+            return View();
+        }
+    }
+}
