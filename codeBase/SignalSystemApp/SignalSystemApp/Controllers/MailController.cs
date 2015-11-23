dd4113c5239e0bfc7fd2238cf0ea099754c09a8c
aljubayer <aljubayer@gmail.com>
2015-11-24 02:00:30 +0600

    mail
    ses hoe nai..sorry ...

--- codeBase/SignalSystemApp/SignalSystemApp/Controllers/MailController.cs
+++ codeBase/SignalSystemApp/SignalSystemApp/Controllers/MailController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SignalSystemApp.Models;
+using SignalSystemApp.Models.Mail;
 
 namespace SignalSystemApp.Controllers
 {
@@ -16,6 +18,70 @@ namespace SignalSystemApp.Controllers
         public ActionResult ListMails()
         {
             return View();
+        }
+
+        public ActionResult AddNewMail()
+        {
+            try
+            {
+                string mailId = Request["mailId"].ToString();
+                string mailDescription = Request["mailDescription"].ToString();
+
+                if (mailId.Trim().Length == 0 || mailDescription.Trim().Length == 0)
+                {
+                    ViewData["message"] = "Error! Fields shold not be empty.";
+                    return View("Index");
+                }
+
+                Mail aMail = new Mail();
+                string message = aMail.AddNewMail(mailId.Trim(), mailDescription.Trim());
+                ViewData["message"] = message;
+                return View("Index");
+            }
+            catch (Exception exception)
+            {
+                ViewData["message"] = "Error! " + exception.Message;
+                return View("Index");
+            }
+        }
+
+        public JsonResult DataProviderAction(JQueryDataTableParamModel aModel)
+        {
+
+            Mail aMail = new Mail();
+
+            List<MailData> mailData = aMail.GetMailData();
+
+            var result = from aMailData in mailData
+                select new[]
+                {
+
+                    aMailData.ID,
+                    aMailData.MailID,
+                    aMailData.MailDescription,
+                    aMailData.MailDate.ToString("dd-MM-yyyy").ToString()
+
+
+
+                };
+
+
+            List<string[]> resultList = result.ToList();
+            List<string> aRankList = new List<string>();
+            List<string> banumberList = new List<string>();
+            List<string> complainTypeList = new List<string>();
+            return Json(new
+            {
+                sEcho = aModel.sEcho,
+                //yadcf_data_0 = banumberList,
+                //yadcf_data_2 = aRankList,
+                //yadcf_data_5 = complainTypeList,
+
+                iTotalRecords = 12,//complanList.Count(),
+                iTotalDisplayRecords = 10,//filteredComplaneList.Count(),
+                aaData = resultList
+            },
+               JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ codeBase/SignalSystemApp/SignalSystemApp/Models/Mail/Mail.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using SignalSystem.Libs;
+
+namespace SignalSystemApp.Models.Mail
+{
+    public class MailData
+    {
+        public string ID { set; get; }
+        public string MailID { set; get; }
+        public string MailDescription { set; get; }
+        public DateTime MailDate { set; get; }
+    }
+    public class Mail
+    {
+        DBGateway aGateway = new DBGateway("SignalSystemConnectionString");
+        public string AddNewMail(string mailId, string mailDescription)
+        {
+            string insertString = "INSERT INTO maildata (`MailID`, `MailDescription`,`Date`) VALUES (@MailID, @mailDescription,@date); ";
+            Hashtable aHashtable = new Hashtable()
+            {
+                {"MailID",mailId},
+                {"mailDescription",mailDescription},
+                {"date",DateTime.Now.ToString("yyyy-MM-dd")}
+            };
+            int lastInsertId = aGateway.Insert(insertString, aHashtable);
+
+            return "Data Successfully Inserted.";
+        }
+
+        public List<MailData> GetMailData()
+        {
+            List<MailData> aList = new List<MailData>();
+            DataSet aSet = aGateway.Select("select * from maildata;");
+
+            foreach (DataRow dataRow in aSet.Tables[0].Rows)
+            {
+                MailData aData = new MailData();
+                aData.ID = dataRow["ID"].ToString();
+                aData.MailDate = Convert.ToDateTime(dataRow["Date"].ToString());
+                aData.MailDescription = dataRow["MailDescription"].ToString();
+                aData.MailID = dataRow["MailID"].ToString();
+                aList.Add(aData);
+            } 
+            return aList; 
+        }
+    }
+}
