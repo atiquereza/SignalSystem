6d83c08284915e196e360556e69f481d6eebff1e
Jubayer <aljubayer@gmail.com>
2015-11-24 16:32:33 +0600

    Beta Version Released from Jubayer, Check

---
+++ codeBase/SignalSystemApp/SignalSystemApp/Controllers/ApplicationController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using SignalSystemApp.Models.Application;
+
+namespace SignalSystemApp.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        // GET: Application
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddNewComplainType()
+        {
+            try
+            {
+                string complain = Request["complainType"].ToString();
+
+                if (complain.Trim().Length == 0)
+                {
+                    ViewData["message"] = "Error! Enter Valid Complain Type.";
+                    return View("Index");
+                }
+
+                ApplicationConfiguration applicationConfiguration = new ApplicationConfiguration();
+                string messasge = applicationConfiguration.AddNewComplainType(complain);
+
+
+
+                ViewData["message"] = "Data Successfully Added.";
+                return View("Index");
+            }
+            catch (Exception exception)
+            {
+                ViewData["message"] = "Error! Exception Found: " + exception.Message;
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ codeBase/SignalSystemApp/SignalSystemApp/Models/Application/ApplicationConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using SignalSystem.Libs;
+
+namespace SignalSystemApp.Models.Application
+{
+    public class ApplicationConfiguration
+    {
+        DBGateway aGateway = new DBGateway("SignalSystemConnectionString");
+        public string AddNewComplainType(string complainType)
+        {
+            string message = string.Empty;
+            if (AlreadyHasThisComplainType(complainType))
+            {
+                return "Error! Complain Type Already Exists.";
+            }
+
+            //string queryString = "select * from ";
+            string insertString = "insert into menucomplaintype (value,name) values(@value,@name)";
+            Hashtable aHashtable = new Hashtable()
+            {
+                {"value",complainType},
+                {"name","Complain"}
+            };
+            int lastInsertId = aGateway.Insert(insertString, aHashtable);
+            return "Data Successfully Added";
+        }
+
+        private bool AlreadyHasThisComplainType(string complainType)
+        {
+            string query = "select * from menucomplaintype where value=@value";
+            Hashtable aHashtable = new Hashtable()
+            {
+                {"value",complainType}
+            };
+
+            DataSet aSet = aGateway.Select(query, aHashtable);
+
+            if (aSet.Tables[0].Rows.Count > 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
