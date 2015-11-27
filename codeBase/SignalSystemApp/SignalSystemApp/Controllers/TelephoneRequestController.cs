b746cc5dcf8e0cfa5c58f80d7e75f1bdc04f287e
aljubayer <aljubayer@gmail.com>
2015-11-28 00:30:39 +0600

    new user, telephone and request links are implemented in controllers and in main layout.

---
+++ codeBase/SignalSystemApp/SignalSystemApp/Controllers/TelephoneRequestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalSystemApp.Controllers
+{
+    public class TelephoneRequestController : Controller
+    {   
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PendingRequest()
+        {
+            return View();
+        }
+        public ActionResult ResolveRequest()
+        {
+            return View();
+        }
+    }
+}
---
+++ codeBase/SignalSystemApp/SignalSystemApp/Controllers/TelephoneUserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalSystemApp.Controllers
+{
+    public class TelephoneUserController: Controller
+    {
+        public ActionResult Index()
+        {
+            //This function will add A new Person Info
+            return View();
+        }
+
+        public ActionResult AddTelephoneUser()
+        {   
+            //dublicate mehtod like Index, this will remove later
+            return View("Index");
+        }
+
+        public ActionResult AddNewTelephoneNumber()
+        {
+            return View();
+        }
+
+        public ActionResult ViewActiveTelephoneUsers()
+        {
+            return View();
+        }
+        public ActionResult ViewTerminatedTelephoneHistory()
+        {
+            return View();
+        }
+
+       
+    }
+}
